9b5b5756ce0888b92d6bc5b5b11c9775c9bb8ba5
Zach Manno <zacharymanno@gmail.com>
2022-03-06 12:17:21 -0500

    weather api call

--- backend/rust_backend_function/src/main.rs
+++ backend/rust_backend_function/src/main.rs
@@ -1,5 +1,11 @@
+mod weather;
+
 use lambda_runtime::{service_fn, LambdaEvent, Error};
 use serde_json::{json, Value};
+use std::env;
+use std::future::Future;
+use reqwest::{Error as req_err, Response};
+use crate::weather::WeatherResponse;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -10,28 +16,45 @@ async fn main() -> Result<(), Error> {
 
 async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let (event, _context) = event.into_parts();
-    let first_name = event["firstName"].as_str().unwrap_or("world");
+    //let first_name = event["firstName"].as_str().unwrap_or("world");
+    let weather_api_key = env::var("API_KEY").unwrap();
+    let weather_data = call_weather_api(&weather_api_key).await;
+
+    let temp = match weather_data {
+        Ok(data) => match data.json::<WeatherResponse>().await {
+            Ok(json_parsed_response) => (format!("{}", json_parsed_response.main.temp)),
+            Err(json_error) => {
+                println!("Problem calling weather api (json parse): {:?}", json_error);
+                String::from("")
+            }
+        },
+        Err(error) => {
+            println!("Problem calling weather api: {:?}", error);
+            String::from("")
+        }
+    };
 
     Ok(
         json!(
-            { "message": format!("Weather is {}", "here!") }
+            {
+                "temperature": format!("{}", temp),
+                "description": format!("{}", "d"),
+                "location": format!("{}", "Philadelphia")
+            }
         )
     )
 }
 
-async fn call_weather_api() -> Result<Response, Error> {
+async fn call_weather_api(api_key: &str) -> Result<Response, Error> {
     // Build the client using the builder pattern
     let client = reqwest::Client::new();
 
     // Perform the actual execution of the network request
     let response = client
-        .get("https://api.openweathermap.org/data/2.5/weather?\
-            appid=dcafd716328a8a378ffa82beeaedae52\
-            &q=Philadelphia%2CUSA&units=imperial")
-        .header("Accept", "application/json, text/plain, */*")
-        .header("Referer", "https://magiceden.io/")
-        .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36")
-        .send().await;
+        .get(format!("https://api.openweathermap.org/data/2.5/weather?\
+            appid={}\
+            &q=Philadelphia%2CUSA&units=imperial", api_key))
+        .send();
 
     return response;
 }
