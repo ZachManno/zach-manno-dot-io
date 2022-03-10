951883ae89f5a33023b259ffc5eec06122eaf731
Zach Manno <zacharymanno@gmail.com>
2022-03-10 17:55:56 -0500

    round decimal

--- zachmanno.io/static/js/scripts.js
+++ zachmanno.io/static/js/scripts.js
@@ -18,7 +18,7 @@ function callWeather() {
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    $("#weatherID").text('It is ' + data.temperature + ' degrees in Philadelphia right now!');
+    $("#weatherID").text('It is ' + Math.round(parseFloat(data.temperature)) + ' degrees in Philadelphia right now!');
   }
   );
 }
