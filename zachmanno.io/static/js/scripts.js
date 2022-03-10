20551aec647f23b1237d1eee0403eb6746a625ba
Zach Manno <zacharymanno@gmail.com>
2022-03-10 17:39:28 -0500

    add weather

--- zachmanno.io/static/js/scripts.js
+++ zachmanno.io/static/js/scripts.js
@@ -5,3 +5,20 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+$(document).ready(function(){
+
+	//=== do some code stuff...
+
+	//===finally, bind my events...
+	callWeather();
+});
+
+function callWeather() {
+  fetch('https://ml20kezqk5.execute-api.us-east-1.amazonaws.com/weather')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    $("#weatherID").text('It is ' + data.temperature + ' degrees in Philadelphia right now!');
+  }
+  );
+}
